1a369c44d25d25634acaf64e3fb7311fc9c9ff33
darshan <darshandumaraliya@gmail.com>
2024-02-19 17:15:53 +0530

    update

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,29 +5,15 @@ export const AppDataSource = new DataSource({
   host: "localhost",
   port: 3306,
   username: "root",
+  // password: "password",
   password: "",
   // database: "ludo_game",
   database: "ludo_game",
-  // entities: ["src/entity/{*.ts, *.js}"],
-  entities: ["dist/entity/*.entity.js"],
+  entities: ["src/entity/{*.ts, *.js}"],
+  // entities: ["dist/entity/*.entity.js"],
   // entities: ["entity/*.entity.{ts,js}"],
   logging: true,
   synchronize: false,
 });
-
-// export const AppDataSource = new DataSource({
-//   type: "mysql",
-//   host: "localhost",
-//   port: 3306,
-//   username: "megha",
-//   password: "Megha@1234",
-//   // database: "ludo_game",
-//   database: "ludo_game",
-//   entities: ["src/entity/{*.ts, *.js}"],
-//   // entities: ["dist/entity/*.entity.js"],
-//   // entities: ["entity/*.entity.{ts,js}"],
-//   logging: true,
-//   synchronize: true,
-// });
 
 export default AppDataSource;
